3186bb9279ed490f3bb05aabb9aee68c436403e4
ya95123 <ya95123@gmail.com>
2022-05-14 21:50:39 +0800

    add card data

--- js/data_frontEnd.js
+++ js/data_frontEnd.js
@@ -17,6 +17,96 @@ const data_fontEnd = {
       "intro": "xxxx",
       "url": "https://event.udn.com/member/2022poke/",
       "imgSrc": "img/fontEndWorks/202204_udnMemberPoke.jpg"
+    },
+    {
+      "time": "2022 / 3",
+      "cate": "event",
+      "cateName": "活動宣傳",
+      "title": "U 粉刮好運",
+      "intro": "xxxx",
+      "url": "https://event.udn.com/member/2022lottery/",
+      "imgSrc": "img/fontEndWorks/202203_udnMemberLottery.jpg"
+    },
+    {
+      "time": "2022 / 2",
+      "cate": "game",
+      "cateName": "遊戲互動",
+      "title": "紅狼人殺｜不會玩的人也可以當上帝",
+      "intro": "xxxx",
+      "url": "https://ya95123.github.io/autoWolf_v1/",
+      "imgSrc": "img/fontEndWorks/202202_autoWolfv1.jpg"
+    },
+    {
+      "time": "2021 / 10",
+      "cate": "event",
+      "cateName": "活動宣傳",
+      "title": "Yes, Girls！Let’s 創造未來！",
+      "intro": "xxxx",
+      "url": "https://udn.com/upf/ubrand/2021_data/TWgirlsday/",
+      "imgSrc": "img/fontEndWorks/202110_udnTWgirlsday.jpg"
+    },
+    {
+      "time": "2021 / 10",
+      "cate": "news",
+      "cateName": "新聞網站",
+      "title": "報時光｜聯合新聞網 改版",
+      "intro": "xxxx",
+      "url": "https://time.udn.com/udntime/index",
+      "imgSrc": "img/fontEndWorks/202110_udnTime.jpg"
+    },
+    {
+      "time": "2021 / 7",
+      "cate": "event",
+      "cateName": "活動宣傳",
+      "title": "前進東京｜力挺中華隊 留言抽好禮",
+      "intro": "xxxx",
+      "url": "https://event.udn.com/bd_2021tokyo/",
+      "imgSrc": "img/fontEndWorks/202107_udnTokyo.jpg"
+    },
+    {
+      "time": "2021 / 7",
+      "cate": "event",
+      "cateName": "活動宣傳",
+      "title": "新北市文化局 WFH背景圖「藝遊新北」",
+      "intro": "xxxx",
+      "url": "https://event.udn.com/culture/picture2021/",
+      "imgSrc": "img/fontEndWorks/202107_udnCulturePicture.jpg"
+    },
+    {
+      "time": "2021 / 4",
+      "cate": "news",
+      "cateName": "新聞網站",
+      "title": "新聞專題｜聯合新聞網",
+      "intro": "xxxx",
+      "url": "https://topic.udn.com/issue/index",
+      "imgSrc": "img/fontEndWorks/202104_udnTopic.jpg"
+    },
+    {
+      "time": "2021 / 1",
+      "cate": "map",
+      "cateName": "地圖功能",
+      "title": "財團法人桃園市原住民族發展基金會：資源地圖",
+      "intro": "xxxx",
+      "url": "https://www.tyipdf.com.tw/?FID=30",
+      "imgSrc": "img/fontEndWorks/202101_tyipdfMap.jpg"
+    },
+    {
+      "time": "2021 / 1",
+      "cate": "event",
+      "cateName": "活動宣傳",
+      "title": "加入橘世代VIP！富活人生下半場",
+      "intro": "xxxx",
+      "url": "https://dc.udn.com/alfred/TEK2011IT003/",
+      "imgSrc": "img/fontEndWorks/202101_udnOrangeVip.jpg"
+    },
+    {
+      "time": "2020 / 8",
+      "cate": "game",
+      "cateName": "遊戲互動",
+      "title": "狄斯俊之手",
+      "intro": "xxxx",
+      "url": "https://ya95123.github.io/vue-decisionHand/#/",
+      "imgSrc": "img/fontEndWorks/202008_decisionHand.jpg"
     }
   ]
 }
