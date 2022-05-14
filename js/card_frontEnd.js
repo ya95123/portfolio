f6394f01073b700457833df23d936062b1417128
ya95123 <ya95123@gmail.com>
2022-05-14 17:26:56 +0800

    card produce temp

---
+++ js/card_frontEnd.js
@@ -0,0 +1,28 @@
+import data_fontEnd from "./data_frontEnd.js"
+const card = document.getElementById("card")
+const dataCard = data_fontEnd.card
+console.log(dataCard)
+
+// 製造卡片
+dataCard.forEach(item => {
+  card.insertAdjacentHTML("beforeEnd",
+    `
+    <div class="card-frame" data-card="${item.cate}">
+      <div class="card-img-frame">
+        <img class="card-img" loading="lazy" src="${item.imgSrc}">
+      </div>
+      <div class="card-tag-frame">
+        <span class="card-tag" data-cate="${item.cate}">活動宣傳</span>
+        <span class="card-time">${item.time}</span>
+      </div>
+      <div class="card-text-frame">
+        <h3 class="card-title">
+          ${item.title}
+          <a class="fa-solid fa-link" href="${item.url}" target="_blank"></a>
+        </h3>
+        <p class="card-intro">${item.intro}</p>
+      </div>
+    </div>
+    `
+  )
+})
---
+++ js/data_frontEnd.js
@@ -0,0 +1,24 @@
+const data_fontEnd = {
+  "card": [
+    {
+      "time": "2022 / 5",
+      "cate": "event",
+      "cateName": "活動宣傳",
+      "title": "NBA 總冠軍賽賓果｜NBA 台灣｜聯合新聞網",
+      "intro": "xxxx",
+      "url": "https://nba.udn.com/finalsbingo/",
+      "imgSrc": "img/fontEndWorks/202205_udnNBA.jpg"
+    },
+    {
+      "time": "2022 / 4",
+      "cate": "event",
+      "cateName": "活動宣傳",
+      "title": "繽紛戳戳樂",
+      "intro": "xxxx",
+      "url": "https://event.udn.com/member/2022poke/",
+      "imgSrc": "img/fontEndWorks/202204_udnMemberPoke.jpg"
+    }
+  ]
+}
+
+export default data_fontEnd
