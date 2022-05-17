bee7a0d998caf86a6c2c4b3db721fc2c221d9f69
ya95123 <ya95123@gmail.com>
2022-05-17 14:45:18 +0800

    add card data intro

--- js/data_frontEnd.js
+++ js/data_frontEnd.js
@@ -6,7 +6,7 @@ const data_fontEnd = {
       "cateName": "活動宣傳",
       "company": "聯合線上",
       "title": "NBA 總冠軍賽賓果｜NBA 台灣｜聯合新聞網",
-      "intro": "xxxx",
+      "intro": "用 JS 做出賓果排序與選擇，再跟後端溝通串接 API 完成賓果功能。5/16 - 5/31 正好是活動期間，可點入參與活動。",
       "url": "https://nba.udn.com/finalsbingo/",
       "imgSrc": "img/fontEndWorks/202205_udnNBA.jpg"
     },
@@ -16,7 +16,7 @@ const data_fontEnd = {
       "cateName": "活動宣傳",
       "company": "聯合線上",
       "title": "繽紛戳戳樂",
-      "intro": "xxxx",
+      "intro": "用 CSS 寫出戳洞動畫，並串接 API 做出與使用者的問答互動、頁面資訊的修改。5/1 - 6/1 正好是活動期間，可點入參與活動。",
       "url": "https://event.udn.com/member/2022poke/",
       "imgSrc": "img/fontEndWorks/202204_udnMemberPoke.jpg"
     },
@@ -26,7 +26,7 @@ const data_fontEnd = {
       "cateName": "活動宣傳",
       "company": "聯合線上",
       "title": "U 粉刮好運",
-      "intro": "xxxx",
+      "intro": "用 JS 做出刮刮卡效果，並串接 API 紀錄遊戲結果、頁面資訊的修改。非活動時間，畫面為預設值。",
       "url": "https://event.udn.com/member/2022lottery/",
       "imgSrc": "img/fontEndWorks/202203_udnMemberLottery.jpg"
     },
@@ -36,7 +36,7 @@ const data_fontEnd = {
       "cateName": "遊戲互動",
       "company": "個人創作",
       "title": "紅狼人殺｜不會玩的人也可以當上帝",
-      "intro": "xxxx",
+      "intro": "因喜歡玩狼人殺而自製的遊戲頁，其功能可取代卡牌的使用(仍需實體見面，非連線遊戲)，並且會有引導提示，跟著步驟大家就能夠順利進行遊戲。",
       "url": "https://ya95123.github.io/autoWolf_v1/",
       "imgSrc": "img/fontEndWorks/202202_autoWolfv1.jpg"
     },
@@ -46,7 +46,7 @@ const data_fontEnd = {
       "cateName": "活動宣傳",
       "company": "聯合線上",
       "title": "Yes, Girls！Let’s 創造未來！",
-      "intro": "xxxx",
+      "intro": "畫面中搭配 svg 動畫、CSS & JS 動態效果，深獲客戶的喜愛。",
       "url": "https://udn.com/upf/ubrand/2021_data/TWgirlsday/",
       "imgSrc": "img/fontEndWorks/202110_udnTWgirlsday.jpg"
     },
@@ -56,7 +56,7 @@ const data_fontEnd = {
       "cateName": "新聞網站",
       "company": "聯合線上",
       "title": "報時光｜聯合新聞網 改版",
-      "intro": "xxxx",
+      "intro": "根據其他新聞網頁面做 Layout 的改版，新增「讀報時光」、「時光機」功能，並加上 Google 廣告。",
       "url": "https://time.udn.com/udntime/index",
       "imgSrc": "img/fontEndWorks/202110_udnTime.jpg"
     },
@@ -66,7 +66,7 @@ const data_fontEnd = {
       "cateName": "活動宣傳",
       "company": "聯合線上",
       "title": "前進東京｜力挺中華隊 留言抽好禮",
-      "intro": "xxxx",
+      "intro": "2021 年東京奧運，使用者可以留言為選手加油打氣，透過 API 串接紀錄文字，用 CSS 搭配 JS 使畫面跳動使用者留言。",
       "url": "https://event.udn.com/bd_2021tokyo/",
       "imgSrc": "img/fontEndWorks/202107_udnTokyo.jpg"
     },
@@ -76,7 +76,7 @@ const data_fontEnd = {
       "cateName": "活動宣傳",
       "company": "聯合線上",
       "title": "新北市文化局 WFH背景圖「藝遊新北」",
-      "intro": "xxxx",
+      "intro": "2021 年因居家生活，而推行了視訊背景圖供使用者下載，並串接 API 紀錄下載次數。",
       "url": "https://event.udn.com/culture/picture2021/",
       "imgSrc": "img/fontEndWorks/202107_udnCulturePicture.jpg"
     },
@@ -86,7 +86,7 @@ const data_fontEnd = {
       "cateName": "新聞網站",
       "company": "聯合線上",
       "title": "新聞專題｜聯合新聞網",
-      "intro": "xxxx",
+      "intro": "整合聯合新聞網的專題內容而製。製作首頁、內容頁模版、串接 API，再交由後端套 php，編輯從後台更新資訊。",
       "url": "https://topic.udn.com/issue/index",
       "imgSrc": "img/fontEndWorks/202104_udnTopic.jpg"
     },
@@ -96,7 +96,7 @@ const data_fontEnd = {
       "cateName": "地圖功能",
       "company": "桃原發",
       "title": "財團法人桃園市原住民族發展基金會：資源地圖",
-      "intro": "xxxx",
+      "intro": "提供桃園原住民的資源地圖。透過串接 Google Map API 將功能寫成，再交由後端套 php 提供後台供編輯人員修改資料。",
       "url": "https://www.tyipdf.com.tw/?FID=30",
       "imgSrc": "img/fontEndWorks/202101_tyipdfMap.jpg"
     },
@@ -106,7 +106,7 @@ const data_fontEnd = {
       "cateName": "活動宣傳",
       "company": "聯合線上",
       "title": "加入橘世代VIP！富活人生下半場",
-      "intro": "xxxx",
+      "intro": "用 CSS、JS 寫出主視覺產出泡泡上升效果，每次重整頁面都會是隨機位置和顏色編排。",
       "url": "https://dc.udn.com/alfred/TEK2011IT003/",
       "imgSrc": "img/fontEndWorks/202101_udnOrangeVip.jpg"
     },
@@ -116,7 +116,7 @@ const data_fontEnd = {
       "cateName": "遊戲互動",
       "company": "個人創作",
       "title": "狄斯俊之手",
-      "intro": "xxxx",
+      "intro": "此為職訓時的專題作品，且成為下幾屆成品範例。使用 Vue Cli 搭配 Vuetify UI 套件完成的互動頁面。提供猜拳做選擇、轉盤、轉酒瓶(手機版)、抽獎功能。",
       "url": "https://ya95123.github.io/vue-decisionHand/#/",
       "imgSrc": "img/fontEndWorks/202008_decisionHand.jpg"
     }
